8c9065737ac6241e1f3f9f4165282fb85b99ffe9
wanggangx <wanggangx@luxin.cn>
2021-02-04 14:15:12 +0800

    提交信息

--- .umirc.ts
+++ .umirc.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'dumi';
 
-export default defineConfig({
+IGC-IoT平台 defineConfig({
   title: 'IGC-IoT平台',
   mode: 'site',
+  base: 'https://poal222.github.io/iGC_webDoc/',
+  publicPath: 'https://poal222.github.io/iGC_webDoc/',
   // more config: https://d.umijs.org/config
 });
